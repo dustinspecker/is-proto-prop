a75fad941396f55aeeebd2ab2d400267b3cfd7c1
Dustin Specker <DustinSpecker@DustinSpecker.com>
2024-05-22 20:47:01 -0500

    chore(package.json): migrate to ESLint 9

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+'use strict'
+const eslintJS = require('@eslint/js')
+const globals = require('globals')
+
+module.exports = [
+  eslintJS.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+    ignores: [
+      "coverage",
+    ],
+  },
+]
